9b5bd79e2a6165e036530e3029061e97a0da8d1d
s27549-pj <s27549@pjwstk.edu.pl>
2024-06-15 22:00:45 +0200

    api gen

---
+++ book-order/src/test/java/com/example/bookorder/BookOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookorder;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
